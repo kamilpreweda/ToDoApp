2e587ab7aea2e89ade30cd6c858e4ff0139a76b7
Kamil Preweda <preweda.kamil@o2.pl>
2022-09-21 16:12:02 +0200

    Installed NuGet packages and created Authentication Controller

---
+++ ToDoAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public AuthenticationController(IConfiguration config)
+    {
+        _config = config;
+    }
+}
