780706add679fa7ca7a07128c502f6f34539ec0b
Kamil Preweda <preweda.kamil@o2.pl>
2022-09-22 17:51:51 +0200

    Added ToDo Controller

---
+++ ToDoAPI/Controllers/ToDosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDosController : ControllerBase
+{
+    // GET: api/ToDos
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET api/<ToDos/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // POST api/<ToDos
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // PUT api/<ToDos/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // PUT api/Todos/5/Complete
+    [HttpPut("{id}/Complete")]
+    public void Complete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // DELETE api/<ToDos/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
