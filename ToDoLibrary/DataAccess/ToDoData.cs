7fc98a1d5d81bb4f7bc6db88c98c2abfc13f7de7
Kamil Preweda <preweda.kamil@o2.pl>
2022-09-27 19:58:26 +0200

    Created Task for GetAllAssigned stored procedure

---
+++ ToDoLibrary/DataAccess/ToDoData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoLibrary.Models;
+
+namespace ToDoLibrary.DataAccess
+{
+    public class ToDoData
+    {
+        private readonly ISqlDataAccess _sql;
+
+        public ToDoData(ISqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+        public Task <List<ToDoModel>> GetAllAssigned(int assignedTo)
+        {
+            return _sql.LoadData<ToDoModel, dynamic>(
+                "dbp.spToDos_GetAllAssigned",
+                new { AssignedTo = assignedTo },
+                "Default");
+        }
+    }
+}
