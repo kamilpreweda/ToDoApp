cbadff19db4713fa9934ae9f03e3cd3f1e6a2428
Kamil Preweda <preweda.kamil@o2.pl>
2022-11-22 18:35:00 +0100

    Enabled authorization directly in Swagger

--- ToDoAPI/StartupConfig/DependencyInjectionExtensions.cs
+++ ToDoAPI/StartupConfig/DependencyInjectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using ToDoLibrary.DataAccess;
 
@@ -36,7 +37,38 @@ namespace ToDoAPI.StartupConfig
         {
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.AddSwaggerServices();
+        }
+        private static void AddSwaggerServices(this WebApplicationBuilder builder)
+        {
+            var securityScheme = new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Description = "JWT Authorization header info using bearer tokens",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT"
+            };
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "bearerAuth"
+                        }
+                    },
+                    new string[] {}
+                }
+            };
+            builder.Services.AddSwaggerGen(opts =>
+            {
+                opts.AddSecurityDefinition("bearerAuth", securityScheme);
+                opts.AddSecurityRequirement(securityRequirement);
+            });
         }
 
         public static void AddCustomServices(this WebApplicationBuilder builder)
